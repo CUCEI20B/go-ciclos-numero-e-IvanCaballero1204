8a66c7582f053bf82fc47a9f1c84a77869388cf1
repl.it user <replituser@example.com>
2020-09-28 18:19:49 +0000

    ciclos numero_e terminado

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var e float64 = 0.0
+
+	factorial := 1.0
+
+	fmt.Scan(&n)
+
+	for i := 0; i <= n; i++ {
+		for j := i; j > 1; j-- {
+			factorial *= float64(j)
+		}
+		e += 1.0 / float64(factorial)
+		factorial = 1
+	}
+	println(e)
 }
